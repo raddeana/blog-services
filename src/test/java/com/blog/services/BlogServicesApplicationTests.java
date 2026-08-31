34a84592446af4409f58b5aa52183f7d55c3078e
陈翔宇 <wowcxy2008@126.com>
2026-08-31 18:12:00 +0800

    fix issues;

--- src/test/java/com/blog/services/BlogServicesApplicationTests.java
+++ src/test/java/com/blog/services/BlogServicesApplicationTests.java
@@ -1,9 +1,11 @@
 package com.blog.services;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith()
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class BlogServicesApplicationTests {
 
